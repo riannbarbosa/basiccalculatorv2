d0c9eaeffbf94d5a80f0a564491916f48e3c5739
Rian Barbosa <rianbbarbosa@outlook.com>
2023-01-16 14:32:47 -0300

    just commited

--- src/test/calcTest.js
+++ src/test/calcTest.js
@@ -6,7 +6,7 @@ import {
   divide, plus, sub, times,
 } from '../calc';
 
-const sum = () => plus(9, 9).catch((error) => error);
+// const sum = () => plus(9, 9).catch(error => error);
 
 const div = () => divide(9, 9).catch((error) => error);
 
@@ -17,42 +17,38 @@ const timess = () => times(9, 9).catch((error) => error);
 describe('Testing calc operations (+,/,-,*)', () => {
   describe('#plus', () => {
     it('1 - Should return the value of  9 + 9, that is equals to 18', async () => {
-      const res = sum();
-      res.then((testRes) => {
-        assert.equal(testRes, 18);
-        // eslint-disable-next-line no-undef
-        done();
-      });
+      assert.equal(await plus(9, 9), 18);
+      done();
     });
   });
+});
 
-  describe('#divide', () => {
-    it('2 - Should return the value of  9 / 9, that is equals to 1', async () => {
-      const res = div();
-      res.then((testRes) => {
-        assert.equal(testRes, 1);
-        done();
-      });
+describe('#divide', () => {
+  it('2 - Should return the value of  9 / 9, that is equals to 1', async () => {
+    const res = div();
+    res.then((testRes) => {
+      assert.equal(testRes, 1);
+      done();
     });
   });
+});
 
-  describe('#sub', () => {
-    it('3 - Should return the value of  9 - 9, that is equals to 0', async () => {
-      const res = subtract();
-      res.then((testRes) => {
-        assert.equal(testRes, 0);
-        done();
-      });
+describe('#sub', () => {
+  it('3 - Should return the value of  9 - 9, that is equals to 0', async () => {
+    const res = subtract();
+    res.then((testRes) => {
+      assert.equal(testRes, 0);
+      done();
     });
   });
+});
 
-  describe('#times', () => {
-    it('4 - Should return the value of  9 * 9, that is equals to 81', async () => {
-      const res = timess();
-      res.then((testRes) => {
-        assert.equal(testRes, 81);
-        done();
-      });
+describe('#times', () => {
+  it('4 - Should return the value of  9 * 9, that is equals to 81', async () => {
+    const res = timess();
+    res.then((testRes) => {
+      assert.equal(testRes, 81);
+      done();
     });
   });
 });
