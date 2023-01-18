95834fe322b3cbeaa9f9703c2998f63a382ad373
Rian Barbosa <rianbbarbosa@outlook.com>
2023-01-18 14:32:47 -0300

    changes in calc/

--- src/calc/plus.js
+++ src/calc/plus.js
@@ -6,16 +6,16 @@ const plus = async (x, y) => {
   const encoded = `https://api.mathjs.org/v4/?expr=${x}%2B${y}`;
   try {
     const response = await axios.get(encoded);
+
+    if (response.status === 400) throw new Error('Erro na API ');
     console.log(response.data);
     return response.data;
   } catch (err) {
-    if (err.response) {
-      console.error(err.response.data);
-      console.error(err.response.status);
-      console.error(err.response.headers);
-    }
-    if (err.request) console.log('GET request error', err.message);
-    console.error(err.config);
+    console.error(err.response.data);
+    console.error(err.response.headers);
+
+    if (err.request) console.error(`GET request error ${err.request}`);
+    throw err;
   }
 };
 export default plus;
--- src/calc/times.js
+++ src/calc/times.js
@@ -6,16 +6,16 @@ const times = async (x, y) => {
   const encoded = `https://api.mathjs.org/v4/?expr=${x}*${y}`;
   try {
     const response = await axios.get(encoded);
+
+    if (response.status === 400) throw new Error('Erro na API (400) ');
     console.log(response.data);
     return response.data;
   } catch (err) {
-    if (err.response) {
-      console.error(err.response.data);
-      console.error(err.response.status);
-      console.error(err.response.headers);
-    }
-    if (err.request) console.error('GET request error', err.message);
-    console.error(err.config);
+    console.error(err.response.data);
+    console.error(err.response.headers);
+
+    if (err.request) console.error(`GET request error ${err.request}`);
+    throw err;
   }
 };
 export default times;
